de957e38bd6e39874392f93885485489821b53c3
Vitalii Kizlov <Vitalii.Kizlov@deltatre.com>
2020-06-15 12:44:08 +0300

    Initial Commit

---
+++ PageDesignTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PageDesignTest
+//
+//  Created by Vitalii Kizlov on 15.06.2020.
+//  Copyright © 2020 Vitalii Kizlov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
